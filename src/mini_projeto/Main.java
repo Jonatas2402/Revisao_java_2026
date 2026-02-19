7d8cdaf0385b5b2160110c506ce1de86af9a183d
jonatas <jonatas_dev@hotmail.com>
2026-02-18 23:52:57 -0300

    TODO: teoria polimorfismo.

--- src/mini_projeto/Main.java
+++ src/mini_projeto/Main.java
@@ -1,7 +1,12 @@
 package mini_projeto;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+
 
     }
 }
