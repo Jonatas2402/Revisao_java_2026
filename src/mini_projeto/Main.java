c9b84e0a12257ab43caa978313d3df65da415959
jonatas <jonatas_dev@hotmail.com>
2026-02-20 14:16:15 -0300

    TODO: Iniciando mini projeto

--- src/mini_projeto/Main.java
+++ src/mini_projeto/Main.java
@@ -8,9 +8,23 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc  = new Scanner(System.in);
-
+        //Utilizar Scanner para entrar com os dados.
+        System.out.println("Cadastro de usuários");
+        int escolha = sc.nextInt();
         List<Usuario> listaDeUsuarios = new ArrayList<>();
-
-
+        //Lista para salvar os usuários.
+        //Criar um while para fazer a lógica de criar, remover, listar entre outros.
+        while (escolha != 4){
+            switch (escolha){
+                case 1:
+                    System.out.println("Criando usuario.");
+                    System.out.print("Nome: ");
+                    String nome = sc.nextLine();
+                    Usuario user = new Usuario(nome);
+                    listaDeUsuarios.add(user);
+                    break;
+                case 2:
+            }
+        }
     }
 }
--- src/mini_projeto/Usuario.java
+++ src/mini_projeto/Usuario.java
@@ -3,4 +3,15 @@ package mini_projeto;
 public class Usuario {
     private String nome;
 
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
